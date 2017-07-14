3d4f9214eb40fe827f1db1ba57d97bbb16564050
Daniel Szopa <szopanator@gmail.com>
2017-07-13 21:21:11 -0500

    Initial Commit

---
+++ src/main/kotlin/Chat.kt
@@ -0,0 +1,47 @@
+
+
+import j2html.TagCreator.*
+import org.eclipse.jetty.websocket.api.Session
+import org.json.JSONObject
+import spark.Spark.*
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.function.Predicate
+
+object Chat {
+
+    // this map is shared between sessions and threads, so it needs to be thread-safe (http://stackoverflow.com/a/2688817)
+    internal var userUsernameMap: ConcurrentHashMap<Session, String> = ConcurrentHashMap()
+    internal var nextUserNumber = 1 //Assign to username for next connecting user
+
+    @JvmStatic fun main(args: Array<String>) {
+        staticFiles.location("/public") //index.html is served at localhost:4567 (default port)
+        staticFiles.expireTime(600)
+        webSocket("/chat", ChatWebSocketHandler::class.java)
+        init()
+    }
+
+    //Sends a message from one user to all users, along with a list of current usernames
+    fun broadcastMessage(sender: String, message: String) {
+        userUsernameMap.keys.stream().filter(Predicate<Session> { it.isOpen() }).forEach { session ->
+            try {
+                session.remote.sendString(JSONObject()
+                        .put("userMessage", createHtmlMessageFromSender(sender, message))
+                        .put("userlist", userUsernameMap.values).toString())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    //Builds a HTML element with a sender-name, a message, and a timestamp,
+    private fun createHtmlMessageFromSender(sender: String, message: String): String {
+        return article(
+                b(sender + " says:"),
+                span(attrs(".timestamp"), SimpleDateFormat("HH:mm:ss").format(Date())),
+                p(message)
+        ).render()
+    }
+
+}
---
+++ src/main/kotlin/ChatWebSocketHandler.kt
@@ -0,0 +1,37 @@
+import org.eclipse.jetty.websocket.api.Session
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage
+import org.eclipse.jetty.websocket.api.annotations.WebSocket
+
+@WebSocket
+class ChatWebSocketHandler {
+
+    private var sender: String? = null
+    private var msg: String? = null
+
+    @OnWebSocketConnect
+    @Throws(Exception::class)
+    fun onConnect(user: Session) {
+        Chat.nextUserNumber = Chat.nextUserNumber + 1
+        val username = "User" + Chat.nextUserNumber
+        Chat.userUsernameMap.put(user, username)
+        Chat.broadcastMessage("Server", username + "joined the chat")
+    }
+
+    @OnWebSocketClose
+    fun onClose(user: Session, statusCode: Int, reason: String) {
+        val username = Chat.userUsernameMap[user]
+        Chat.userUsernameMap.remove(user)
+        Chat.broadcastMessage("Server",username + " left the chat")
+    }
+
+    @OnWebSocketMessage
+    fun onMessage(user: Session, message: String) {
+        // TODO: i think it makes sense to send messages as json strings, this way pathing can easily be implemented, as well as headers
+        val user = Chat.userUsernameMap[user]
+        if (user != null) {
+            Chat.broadcastMessage(user, message)
+        }
+    }
+}
