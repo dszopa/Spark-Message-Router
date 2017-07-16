506541affc8bef591d76297f1cb3767e2e6111a5
Daniel Szopa <szopanator@gmail.com>
2017-07-15 21:29:07 -0500

    Change to use Kotlin reflection instead of Java

--- src/main/kotlin/io/dszopa/github/message_router/MessageRouter.kt
+++ src/main/kotlin/io/dszopa/github/message_router/MessageRouter.kt
@@ -5,13 +5,17 @@ import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner
 import io.github.lukehutch.fastclasspathscanner.scanner.ScanResult
 import org.eclipse.jetty.websocket.api.Session
 import org.json.JSONObject
-import java.lang.annotation.AnnotationTypeMismatchException
-import java.lang.reflect.Method
 import java.util.*
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.declaredFunctions
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.jvm.jvmErasure
 
 class MessageRouter(packagePath: String) {
 
-    private val routeToMethodCalls: HashMap<String, Pair<Any, Method>> = HashMap()
+    private val routeToMethodCalls: HashMap<String, Pair<Any, KFunction<*>>> = HashMap()
 
     init {
         val scanResult: ScanResult = FastClasspathScanner(packagePath)
@@ -21,19 +25,22 @@ class MessageRouter(packagePath: String) {
         val classesContainingRouteMethods: List<String> = scanResult.getNamesOfClassesWithMethodAnnotation(Route::class.java)
 
         for (className: String in classesContainingRouteMethods) {
+            val clazz: KClass<*> = Class.forName(className).kotlin
+            val instance: Any = clazz.createInstance()
 
-            val clazz: Class<*> = Class.forName(className)
-            val instance: Any = clazz.newInstance()
+            for (function: KFunction<*> in clazz.declaredFunctions) {
+                if (function.annotations.any { it.annotationClass == Route::class}) {
 
-            for (method: Method in clazz.getDeclaredMethods()) {
-                if (method.isAnnotationPresent(Route::class.java)) {
-                    // Throw exception if Annotation doesn't have correct parameters
-                    val parameterTypes: List<Class<*>> = method.parameterTypes.asList()
-                    if (!parameterTypes.contains(String::class.java) || !parameterTypes.contains(Session::class.java)) {
-                        throw  AnnotationTypeMismatchException(method, "Missing String and / or Session parameter types")
+                    if (!function.parameters.any { it.type.jvmErasure == String::class } ||
+                            !function.parameters.any { it.type.jvmErasure == Session::class }) {
+                        throw Error("Missing String and / or Session parameter types on function: ${function.name}")
                     }
-                    val route: Route = method.getAnnotation(Route::class.java)
-                    routeToMethodCalls.put(route.value, Pair(instance, method))
+
+                    val route: Route? = function.findAnnotation<Route>() // We already confirmed that it has a route annotation
+                    if (route != null) {
+                        val value: String = route.value
+                        routeToMethodCalls.put(value, Pair(instance, function))
+                    }
                 }
             }
         }
@@ -41,10 +48,11 @@ class MessageRouter(packagePath: String) {
 
     fun handle(user: Session, message: String) {
         val messageJson: JSONObject = JSONObject(message)
+
         if (messageJson.has("route") && routeToMethodCalls.containsKey(messageJson.getString("route"))) {
             val route: String = messageJson.getString("route")
-            val methodPair: Pair<Any, Method>? = routeToMethodCalls[route]
-            methodPair?.second?.invoke(methodPair.first, user, message)
+            val methodPair: Pair<Any, KFunction<*>>? = routeToMethodCalls[route]
+            methodPair?.second?.call(methodPair.first, user, message)
         }
     }
 }
